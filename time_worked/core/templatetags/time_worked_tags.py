c0a408e0c92b63214d8f4890ba2dd1193ed7e7c2
Moreno Cunha <moreno.pinheiro@gmail.com>
2018-03-16 22:06:02 -0300

    Add a custom template tag

---
+++ time_worked/core/templatetags/time_worked_tags.py
@@ -0,0 +1,32 @@
+from django import template
+
+register = template.Library()
+
+
+
+@register.simple_tag
+def get(qs, **kwargs):
+    """Template tag which allows queryset get.
+    Usage:
+      {% get QuerySet field=value as object %}
+      {{ object.name }}
+    """
+    try:
+        return qs.get(**kwargs)
+    except Exception:
+        return None
+
+
+@register.simple_tag
+def filter(qs, **kwargs):
+    """Template tag which allows queryset filter.
+    Usage:
+      {% filter QuerySet field=value as filtered_queryset %}
+      {% for object in filtered_queryset %}
+        {{ object.name }}
+      {% endfor %}
+    """
+    try:
+        return qs.filter(**kwargs)
+    except Exception:
+        return None
--- time_worked/core/views.py
+++ time_worked/core/views.py
@@ -1,5 +1,11 @@
+import datetime
 from django.views.generic import ListView
-from core.models import Employee
+from core.models import Employee, Project
 
 class TimeWorkedList(ListView):
-    model = Employee
+    model = Project
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['yesterday'] = datetime.date.today() - datetime.timedelta(days=1)
+        return context

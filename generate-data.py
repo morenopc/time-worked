f84d6e2cb4bd9b0e2a9ee40591917342291d10c8
Moreno Cunha <moreno.pinheiro@gmail.com>
2018-03-19 16:09:59 -0300

    Add expetions to generate data script file

--- generate-data.py
+++ generate-data.py
@@ -35,8 +35,6 @@ for date in worked_days:
         for employee in Employee.objects.all():
             hours = random.choice(times)
             if hours == 0:
-                print(project)
-                print(employee)
                 break
             time_worked = TimeWorked()
             time_worked.date = date
@@ -44,3 +42,51 @@ for date in worked_days:
             time_worked.employee = employee
             time_worked.time = datetime.time(hours)
             time_worked.save()
+
+# Exceptions
+# Employee didn't work anyday
+fullname = "Didn'tWork Anyday"
+first, last = fullname.split()
+employee = Employee()
+employee.user = User.objects.create_user(
+        first_name=first,
+        last_name=last,
+        username=first.lower(),
+        email='%s@mail.com' % first.lower(),
+        password='&mpl0yiiS')
+employee.save()
+
+# and it's in all projects
+projects = Project.objects.all()
+for project in projects:
+    project.employees.add(employee)
+
+# Employee only worked in Project C
+fullname = 'Only ProjectC'
+first, last = fullname.split()
+employee = Employee()
+employee.user = User.objects.create_user(
+        first_name=first,
+        last_name=last,
+        username=first.lower(),
+        email='%s@mail.com' % first.lower(),
+        password='&mpl0yiiS')
+employee.save()
+
+# only project C
+project = Project.objects.get(name='Project C')
+project.employees.add(employee)
+
+for date in worked_days:
+    time_worked = TimeWorked()
+    time_worked.date = date
+    time_worked.project = project
+    time_worked.employee = employee
+    time_worked.time = datetime.time(random.choice([4, 6, 8]))
+    time_worked.save()
+
+# A project withou employees
+project_name = 'Project F'
+project = Project()
+project.name = project_name
+project.save()
